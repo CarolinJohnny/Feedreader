52307767a1f918f499d091a4617791268ad85206
CarolinJohnny <carolinjohnny@googlemail.com>
2019-04-15 16:15:42 +0200

    Refactor test menu hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,7 +55,6 @@ $(function() {
   /* TODO: Write a new test suite named "The menu" */
   describe('The menu', function() {
 
-  });
   /* TODO: Write a test that ensures the menu element is
    * hidden by default. You'll have to analyze the HTML and
    * the CSS to determine how we're performing the
@@ -71,9 +70,13 @@ $(function() {
    * clicked and does it hide when clicked again.
    */
   it('menu changes visibility', function() {
+    $('.menu-icon-link').trigger.('click');
+    expect($('body').hasClass('menu-hidden')).toBe(false);
 
-  });
-
+    $('.menu-icon-link').trigger.('click');
+    expect($('body').hasClass('menu-hidden')).toBe(true);
+    });
+});
   /* TODO: Write a new test suite named "Initial Entries" */
   describe('Initial Entries', function() {
 
